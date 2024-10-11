69553ee943bbc96495ea3a2aa7158b6e64e2027d
Stefanos <spalaio@moby-it.com>
2024-10-11 12:13:42 +0300

    Added navigation list items to the Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import classes from "../styles/Navbar.module.css";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,14 +9,26 @@ export default function NavBar() {
   return (
     <section className={classes.container}>
       <section className={classes.navbar}>
-        <Link href="/">
-          <Image src="/logonetflix.png" alt="Netflix" width={100} height={40} />
-        </Link>
-        {pathname !== "/login" && (
-          <Link href="/login" className={classes.login}>
-            <h1>Sign-in</h1>
+        <section className={classes.leftSection}>
+          <Link href="/">
+            <img src="/logonetflix.png" alt="Netflix" width={80} height={40} />
           </Link>
-        )}
+          <ul>
+            <li>Home</li>
+            <li>TV shows</li>
+            <li>Movies</li>
+            <li>News & Popular</li>
+            <li>My list</li>
+          </ul>
+        </section>
+        <section className={classes.rightSection}>
+          \
+          {pathname !== "/login" && (
+            <Link href="/login" className={classes.login}>
+              <h1>Sign-in</h1>
+            </Link>
+          )}
+        </section>
       </section>
     </section>
   );
